d92b4178ba30c6046d3ec463eb93ec0ac4ffb818
oripaso <120463766+oripaso@users.noreply.github.com>
2023-06-18 16:39:13 +0300

    added cart page ans small change in mainpage

--- app.js
+++ app.js
@@ -13,18 +13,17 @@ app.use('/cssFiles', express.static(__dirname + '/src/views/cssFiles'));
 
 
 
-mongoose.connect('mongodb://localhost:27017/E-commerce',{ useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/E-commerce', { useNewUrlParser: true });
 
 
 //routes
-app.use('/',require(__dirname+'/src/routes/loginRoute'));
-app.use('/',require(__dirname+'/src/routes/registerRoute'));
-app.use('/',require(__dirname+'/src/routes/mainPageRoute'))
+app.use('/', require(__dirname + '/src/routes/loginRoute'));
+app.use('/', require(__dirname + '/src/routes/registerRoute'));
+app.use('/', require(__dirname + '/src/routes/mainPageRoute'))
+app.use('/', require(__dirname + '/src/routes/cartRoute'));
 
 
 
-
-app.listen(3000,function()
-{
+app.listen(3000, function () {
   console.log("Server listening");
 })
