081d3cbbce17d15e16276d954ffd53496e002838
shaynoadotan <118252959+shaynoadotan@users.noreply.github.com>
2023-07-02 18:04:29 +0300

    added orders table and moved some of the html contents to be generated in the js file

--- src/views/jsFiles/admin.js
+++ src/views/jsFiles/admin.js
@@ -5,14 +5,42 @@ function populateProductTable() {
         // populate the products table
         success: function (res) {
             const { products } = res;
-            const tableBody = $("#products #productsTable tbody");
 
-            // Clear existing rows
-            tableBody.empty();
-        
+            const div = $("#products");
+            div.empty();
+            div.append(`<button id="addProductBtn" class="btn btn-success">Add new product</button>`);
+            div.append(`<button id="submitProductsBtn" class="btn btn-success">Submit new products</button>`);
+            div.append(`<table id="productsTable" class="table"></table>`);
+
+            if (!products || products.length == 0) {
+                return;
+            }
+
+            const table = $("#products #productsTable");
+
+            const headers = 
+                Object.keys(products[0])
+                    .filter(key => !key.startsWith('_'))
+                    .map(key => `<th>${key}</th>`)
+                    .join('');
+            const tableStructure = `
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Color</th>
+                    <th>Type</th>
+                    <th>Image</th>
+                </tr>
+            </thead>
+            <tbody></tbody>`;
+            table.append(tableStructure);
+            const tableBody = $("#products #productsTable tbody");
+            
             // Add new rows
-            for (var i = 0; i < products.length; i++) {
-                const product = products[i];
+            products.forEach(product => {
                 const row = `
                     <tr id="${product._id}">
                         <td><input name="title" type='text' class='form-control' value='${product.title}'></td>
@@ -26,10 +54,79 @@ function populateProductTable() {
                         <td><button class='btn btn-sm btn-danger delete-button' id='${product._id}_delete'>Delete</button></td>
                     </tr>`;
                 tableBody.append(row);
+            });
+        },
+    });
+}
+
+function populateOrderTable(search) {
+    $.ajax({
+        url: `/admin/getAllOrders`,
+        method: 'GET',
+        success: function (res) {
+            const { orders } = res;
+            const div = $("#orders");
+            div.empty();
+            var form = $('<form class="form-inline" id="form-inline"></form>');
+            var searchInput = $('<input class="form-control mr-sm-2" id="order-search-input" type="search" placeholder="Search" aria-label="Search">');
+            var searchButton = $('<button class="btn btn-outline-success my-2 my-sm-0" id="order-search-button" type="submit">Search</button>');          
+            form.append(searchInput);
+            form.append(searchButton);
+            div.append(form);
+
+            $('#order-search-button').click(function (event) {
+                event.preventDefault();
+                const inputButton = $('#order-search-input');
+                const input = inputButton.val();
+                populateOrderTable(input);
+            });
+
+            if (!orders || orders.length == 0) {
+                return;
             }
 
-            const productsDiv = $("#products");
-            productsDiv.css('display', 'block');
+            div.append(`<table id="ordersTable" class="table"></table>`);
+            const table = $("#orders #ordersTable");
+
+            const tableStructure = `
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Created</th>
+                        <th>Price</th>
+                        <th>Products</th>
+                        <th>ConfirmationStatus</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            `;
+            table.append(tableStructure);
+            const tableBody = $("#orders #ordersTable tbody");
+            
+            // Add new rows
+            orders.forEach(order => {
+                order.carts.forEach(cart => {
+                    const isPending = cart.confirmationStatus === 'Pending';
+                    const row = `
+                    <tr id="${order._id}_${order.created}">
+                        <td>${order.username}</td>
+                        <td>${cart.created}</td>
+                        <td>${cart.price}</td>
+                        <td>${cart.products.join(', ')}</td>
+                        <td>${cart.confirmationStatus}</td>
+                        <td><button ${!isPending ? 'disabled' : ''} class='btn btn-sm btn-info confirm-button' id='${order._id}_${cart.created}_confirm'>Confirm</button></td>
+                        <td><button ${!isPending ? 'disabled' : ''} class='btn btn-sm btn-danger cancel-button' id='${order._id}_${cart.created}_cancel'>Cancel</button></td>
+                    </tr>`;
+                    if (!search 
+                        || order.username.includes(search) 
+                        || cart.products.find(product => product.includes(search))) {
+                        tableBody.append(row);
+                    }
+                });
+            });
+
+            const ordersDiv = $("#orders");
+            ordersDiv.css('display', 'block');
         },
     });
 }
@@ -37,6 +134,7 @@ function populateProductTable() {
 $(document).ready(function () {
     // admin clicks on "products" button
     $("#productListingBtn").click(function () {
+        clearPage();
         populateProductTable();
     });
 
@@ -53,6 +151,11 @@ $(document).ready(function () {
                 <td><input name="image" type='text' class='form-control' value=''></td>
             </tr>`;
         tableBody.prepend(row);
+    });
+
+    $('#ordersBtn').click(function() {
+        clearPage();
+        populateOrderTable();
     });
 
     $('#submitProductsBtn').click(function() {
@@ -85,6 +188,30 @@ $(document).ready(function () {
         }); 
 
         populateProductTable();
+    });
+
+    $("#orders").on('click', 'button', function() {
+        const [orderId, created, action] = $(this).attr('id').split('_');
+        if (!orderId || !created || !action) {
+            return;
+        }
+
+        const confirmationStatus = 
+            action === 'confirm'
+            ? 'Confirmed'
+            : 'Cancelled';
+        $.ajax({
+            url: '/admin/updateOrder',
+            method: 'POST',
+            data: JSON.stringify({
+                _id: orderId,
+                created,
+                confirmationStatus
+            }),
+            contentType: 'application/json'
+        }); 
+
+        populateOrderTable();
     });
 
     $("#productsTable").on('click', 'button', function () {
@@ -123,4 +250,10 @@ $(document).ready(function () {
             }); 
         }
     })
+
+    // function to clear the page content
+    function clearPage() {
+        $("#products").empty();
+        $("#orders").empty();
+    }
 });
